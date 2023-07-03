bcf00546fb2aa1cd3340251d7f30cfc5d434847e
Chavis Rujichan <chavisr_pro@vistec.ac.th>
2023-07-03 12:08:00 +0700

    refactor

--- w1/app.py
+++ w1/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_geek():
-    return '<h1>Your first flask server!</h2>'
+    return "<h1>Your first flask server!</h2>"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
